4e067dd2edf41a0f19f61da5fd312d1a90c7ba37
Sen Zhang <szhang.hust@gmail.com>
2019-03-26 22:15:54 +0800

    --amend

--- linux/batch_op.sh
+++ linux/batch_op.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-exit
 
 # batch add suffix
-for i in * ; do mv $i $i".txt" ; done;
+for i in * ; do echo mv $i $i".txt" ; done;
 
 # batch remove suffix
-for i in *.bak; do mv $i ${i%.*}; done;
+for i in *.bak; do echo mv $i ${i%.*}; done;
 # or
-for i in $(find *.bak); do mv $i ${i%.*}; done;
+for i in $(find *.bak); do echo mv $i ${i%.*}; done;
