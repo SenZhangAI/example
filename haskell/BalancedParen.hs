c6b9bbdd036055db6d4f9052ecf8a97e8f9562e8
Sen Zhang <szhang.hust@gmail.com>
2019-08-10 16:30:50 +0800

    fix warning

--- haskell/BalancedParen.hs
+++ haskell/BalancedParen.hs
@@ -1,4 +1,3 @@
--- ((())))) 查看括号是否匹配
 import           Data.Semigroup as Sem
 
 data B = B !Int !Int
@@ -12,18 +11,21 @@ instance Sem.Semigroup B where
 instance Monoid B where
   mempty = B 0 0
 
+parse :: Char -> B
 parse '(' = B 0 1
 parse ')' = B 1 0
 parse _   = B 0 0
 
 
 -- 查看括号是否匹配
+balanced :: Foldable t => t Char -> Bool
 balanced xs = foldMap parse xs == B 0 0
 
 
+main :: IO ()
 main = do
     putStrLn "Enter String and Check if parentheses balanced"
     a <- getLine
-    if (balanced a)
+    if balanced a
        then putStrLn "True"
        else putStrLn "False"
