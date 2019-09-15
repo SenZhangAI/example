5149e72f17ee03039fcf947e550a5053b000bdd9
Sen Zhang <szhang.hust@gmail.com>
2019-09-16 00:34:03 +0800

    reformat

--- haskell/MyFold.hs
+++ haskell/MyFold.hs
@@ -1,21 +1,18 @@
 myFoldl :: (a -> b -> a) -> a -> [b] -> a
-myFoldl _ zero []     = zero
 myFoldl f zero (x:xs) = myFoldl f (f zero x) xs
+myFoldl _ zero []     = zero
 
 myFoldr :: (b -> a -> a) -> a -> [b] -> a
-myFoldr _ zero []     = zero
 myFoldr f zero (x:xs) = f x (myFoldr f zero xs)
+myFoldr _ zero []     = zero
 
 -- implement map by myFoldr
 myMap :: (a -> b) -> [a] -> [b]
 myMap f xs = myFoldr m' [] xs
-  where
-    m' a b = f a : b
+  where m' a b = f a : b
 
 -- implement filter by myFoldr
 myFilter :: (a -> Bool) -> [a] -> [a]
 myFilter f xs = myFoldr filter' [] xs
-  where
-    filter' a b
-      | f a = a : b
-      | otherwise = b
+  where filter' a b | f a = a : b
+                    | otherwise = b
